2fe30af93299ffb93392b50fb1ce6dad3e0d630c
Vitus <83448449+vitustockholm@users.noreply.github.com>
2022-07-03 16:45:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+// JSX
+import BasicSlider from "./components/BasicSlider/BasicSlider";
+import VerticalSlider from "./components/VerticalSlider/VerticalSlider";
+import ZoomSlider from "./components/ZoomSlider/ZoomSlider";
+import SlidesChildrenSlider from "./components/SlidesChildrenSlider/SlidesChildrenSlider";
+import BlendModeSlider from "./components/BlendModeSlider/BlendModeSlider";
+import NavbarSlider from "./components/NavbarSlider/NavbarSlider";
+import ButtonsSlider from "./components/ButtonsSlider/ButtonsSlider";
+import AutoplayButtonSlider from "./components/AutoplayButtonSlider/AutoplayButtonSlider";
+
+const app = () => {
+  return (
+    <div
+      style={{
+        color: "#FFF"
+      }}
+    >
+      <BasicSlider />
+      <VerticalSlider />
+      <ZoomSlider />
+      <SlidesChildrenSlider />
+      <BlendModeSlider />
+      <NavbarSlider />
+      <ButtonsSlider />
+      <AutoplayButtonSlider />
+    </div>
+  );
+};
+
+export default app;
